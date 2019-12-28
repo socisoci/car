369ca34e4c7babd9b042e1fbf6bf2712640bd9af
Administrator <Administrator@PC-12>
2019-12-28 17:27:11 +0800

    car项目

---
+++ WebContent/database.sql
@@ -0,0 +1,21 @@
+create database Car charset=utf8;
+user Car;
+/* Drop Tables */
+
+DROP TABLE NEW_TABLE;
+
+
+
+
+/* Create Tables */
+
+CREATE TABLE NEW_TABLE
+(
+	NAME VARCHAR(20),
+	YANSE VARCHAR(20),
+	GONGLISHU INT,
+	PINGPAI VARCHAR(20)
+);
+
+
+
